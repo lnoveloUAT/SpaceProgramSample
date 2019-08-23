6098794c7495564b584e4756ebe8cae6bf0d61e3
lnoveloUAT <lnovelo@uat.edu>
2019-08-23 00:36:41 -0700

    Initial Commit

---
+++ code.js
@@ -0,0 +1,61 @@
+
+/****************************************************************
+ * Class Declaration
+****************************************************************/
+class InputData {
+
+    constructor(
+        time_seconds,
+        latitude,
+        longitude,
+        gps_altitude,
+        bmpSensor_altitude,
+        bmpSensor_pressure,
+        bmpSensor_temp,
+        digSensor_temp,
+        cssSensor_temp,
+        cssSensor_eCO2,
+        cssSensor_TVOC,
+        UV,
+        accelX,
+        accelY,
+        accelZ,
+        magneticX,
+        magneticY,
+        magneticZ,
+        gyroX,
+        gyroY,
+        gyroZ,
+    ) {
+        this.time_seconds = time_seconds;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.gps_altitude = gps_altitude;
+        this.bmpSensor_altitude = bmpSensor_altitude;
+        this.bmpSensor_pressure = bmpSensor_pressure;
+        this.bmpSensor_temp = bmpSensor_temp;
+        this.digSensor_temp = digSensor_temp;
+        this.cssSensor_temp = cssSensor_temp;
+        this.cssSensor_eCO2 = cssSensor_eCO2;
+        this.cssSensor_TVOC = cssSensor_TVOC;
+        this.UV = UV;
+        this.accelX = accelX;
+        this.accelY = accelY;
+        this.accelZ = accelZ;
+        this.magneticX = magneticX;
+        this.magneticY = magneticY;
+        this.magneticZ = magneticZ;
+        this.gyroX = gyroX;
+        this.gyroY = gyroY;
+        this.gyroZ = gyroZ;
+    }
+}
+
+/****************************************************************
+ * Functions
+****************************************************************/
+function getData(){
+    var loadedData = loadData();
+    return loadedData;
+}
+
